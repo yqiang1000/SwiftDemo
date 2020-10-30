2825ec753b10266d240f111c3baf8321ff085d22
yeqiang <yeqiang@abcpen.com>
2020-10-30 14:59:37 +0800

    add

---
+++ SwiftDemo/Common/SwiftTTPageController/YQHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  YQHeaderView.swift
+//  SwiftDemo
+//
+//  Created by yeqiang on 2020/10/30.
+//
+
+import UIKit
+
+class YQHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
